bb854c73f17df0397937fbe4acafab38f136e752
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-11-29 04:04:59 -0400

    task 1 file manager

---
+++ utils/redis.mjs
@@ -0,0 +1,37 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+import { v4 as uuidv4 } from 'uuid';
+
+class RedisClient {
+  constructor() {
+    // localhost:6739
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`ERROR: ${error}`);
+    });
+
+    this.client.get = promisify(this.client.get);
+    this.client.set = promisify(this.client.set);
+    this.client.del = promisify(this.client.del);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+
+  const redisClient = new RedisClient();
+export default redisClient;
