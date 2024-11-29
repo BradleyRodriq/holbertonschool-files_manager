3553e65517ed3b528a601e8a2483daf2f71ce292
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-11-29 04:09:00 -0400

    task 1 file manager

---
+++ utils/db.mjs
@@ -0,0 +1,33 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+MongoClient.connect = promisify(MongoClient.connect);
+
+class DBClient {
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const dbDatabase = process.env.DB_DATABASE || 'files_manager';
+
+    MongoClient.connect(`mongodb://${dbHost}:${dbPort}`)
+      .then((client) => {
+        this.client = client;
+        this.db = client.db(dbDatabase);
+        this.usersColl = this.db.collection('users');
+        this.filesColl = this.db.collection('files');
+      });
+  }
+
+  isAlive() {
+    if (this.client) {
+      return true;
+    }
+    return false;
+  }
+
+  async nbUsers() {
+    return this.usersColl.countDocuments({});
+  }
+  async nbFiles() {
+    return this.filesColl.countDocuments({});
+  }
+}
